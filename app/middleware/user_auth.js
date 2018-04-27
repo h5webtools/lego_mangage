b37a978094ba8d8a32458d134bc86909986ccd7c
tinghuang <ting.huang@jyblife.com>
2018-04-27 20:14:05 +0800

    release：登录打印日志

--- app/middleware/user_auth.js
+++ app/middleware/user_auth.js
@@ -4,6 +4,7 @@ module.exports = options => {
     const userId = ctx.session.userid;
     const userName = ctx.session.userName;
     let body = ctx.body;
+    this.ctx.logger.info('查看是否更新代码');
     if (ctx.request.url.indexOf('login') === -1 || ctx.request.url.indexOf('syncCallback') === -1 || ctx.request.url.indexOf('previewLock') === -1) {
       await next();
     } else {
