49f5de3a84866abfb91d5eb338f1e774c8cf0abf
zhiming.dai <zhiming.dai@jyblife.com>
2018-06-28 18:58:06 +0800

    legoPageEdit V2 #000

--- web/pages/legoEditV2/app.vue
+++ web/pages/legoEditV2/app.vue
@@ -1,9 +1,9 @@
 <template>
  <el-container>
-   <el-header>
-      <header>www333</header>
-    </el-header>
-    <div class="wrapper">
+
+    <editor-header></editor-header>
+
+    <div class="el-container-lego">
       <editor-layout v-show="activeIndex === 'layout'"></editor-layout>
     </div>
  </el-container>
@@ -18,15 +18,23 @@ import Vue from 'vue'
 
 // component
 import EditorLayout from './modules/layout/index.vue';
+import EditorHeader from './modules/layout/header.vue';
 
 export default {
   components: {
     EditorLayout,
+    EditorHeader
   },
   data() {
     return {
       loading: true,
-      showTestButton: false
+      userName: window.userInfo.userAccount,
+      currentThemeColor: '#fff',
+      themeColor: {
+        color: [],
+        grident: []
+      },
+      currentThemeId: -1,
     };
   },
   computed: {
@@ -79,14 +87,74 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
-.wrapper {
-  height: calc(100vh - 60px);
-  overflow: hidden;
+<style lang="scss">
+
+
+.el-container-lego {
+  margin-top: 59px;
+  height: 100vh;
+  width: 100%;
+
+  .el-aside {
+    background: #FFFFFF;
+    border-right: 1px solid rgba(58,74,167,0.10);
+    overflow: hidden;
+    .components {
+      display: flex;
+      text-align: center;
+      align-items: baseline;
+      flex-wrap: wrap;
+      padding: 20px;
+      .components__item {
+        width: 32%;
+        margin-top: 24px;
+        font-size: 12px;
+        color: #8489AB;
+        border: 1px solid transparent;
+        line-height: 3;
+        &:hover {
+          border: 1px dashed rgba(58,74,167,0.3);
+        }
+        .components__item__imgwrap {
+          width: 88px;
+          height: 66px;
+          margin: 0 auto;
+          background-position: center center;
+          background-size: 100%;
+          background-repeat: no-repeat;
+        }
+      }
+    }
+    .el-collapse-item__content {
+      padding-bottom: 0;
+    }
+    .el-collapse-item__header {
+      background: rgba(58,74,167,0.10);
+      padding-left: 20px;
+      border-bottom: 1px solid rgba(58,74,167,0.3);
+      &.is-active {
+        border-bottom: 0;
+      }
+    }
+  }
 }
-.el-header {
-  padding-left: 0;
-  padding-right: 0;
-  overflow: hidden;
+
+.iphone-container {
+  width: 375px;
+  transition: all 0.3 ease;
+  min-height: 800px;
+  border: 1px dashed #999;
+  position: relative;
+  &:after {
+    content: "iphone6首屏高度667px";
+    position: absolute;
+    width: 100%;
+    border-bottom: 1px dashed #e5e5e5;
+    left: 0;
+    top: 667px;
+    font-size: 12px;
+    color: #e5e5e5;
+    text-align: center;
+  }
 }
 </style>
