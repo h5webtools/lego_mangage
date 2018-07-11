5c3fe797d81a4b5e3b7fee9902caa8daf95d4302
mocept <zhiming.dai@jyblife.com>
2018-07-11 10:21:00 +0800

    fix: legopageV2 使用egg-view #000

--- app/route/v2/lego/index.js
+++ app/route/v2/lego/index.js
@@ -10,6 +10,7 @@ module.exports = app => {
   // const version = config.legoConfigV2.version  
   const version = 'v2'
   const routerPrefix = '/' + version
+  app.router.get(routerPrefix , controller[version].lego.home.index);
   app.router.get(routerPrefix +  '/lego', controller[version].lego.home.index);
   app.router.get(routerPrefix +  '/lego/pageEdit', controller[version].lego.home.index);
   // 获取乐高主题色配置
