ffa63c8c1c48c2b4844f4cb3d95faca15ac9df90
shalizhao <shali.zhao@jyblife.com>
2018-08-16 11:03:30 +0800

    轮播组件

--- app/lego/js/jyb.vue.jybslider.js
+++ app/lego/js/jyb.vue.jybslider.js
@@ -1,110 +1,79 @@
 define(function (require, exports, module) {
+  var _loopSrcoll = require('./loopScroll');
 
-  //var util = require('@lego/util');
-  var util = {
-      getQuery : function (name, url) {
-          var u = url || window.location.search,
-            reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"),
-            r = u.substr(u.indexOf("\?") + 1).match(reg);
-          return r != null ? r[2] : "";
-      },
-      getCookie: function (name) {
-          //读取COOKIE
-          var reg = new RegExp("(^| )" + name + "(?:=([^;]*))?(;|$)"), val = document.cookie.match(reg);
-          return val ? (val[2] ? unescape(val[2]) : "") : null;
-      }
+  var ua = window.navigator.userAgent;
+
+  var ualc = ua.toLowerCase();
+  var isJybApp = /jiayoubao/.test(ualc); // 加油宝app
+
+  var openUrl = function(url) {
+      
   }
+
+  function showLoopScroll(){
+      var headLoopDom = $('#jybwx_banner_slider__container'),
+          items = headLoopDom.find(".wrap-item");
+      _headLoop = _loopSrcoll.init({
+          tp : "img", //图片img或是文字text  默认text
+          moveDom : headLoopDom, //必选  待移动父元素zepto查询对象
+          moveChild : items, //必选  zepto查询对象
+          tab : headLoopDom, //必选  zepto查询对象
+          len : items.length, //总元素
+          index : 1, //当前位移的元素
+          loopScroll : true,
+          tabClass : "on",
+          autoTime:2000,
+          enableTransX : true,
+          fun: function(index) {
+             
+          }
+      });
+  }
+
   function _register(opt) {
 
       Vue.component("jybslider", {
           props: ["params"],
-          // template: '#' + config.uid + "_tpl",
-          /* data: function() {
-               return {
-                   data: {}
-               };
-           },*/
           functional: false,
-          //mixins: [commonFunc.commonUtil.utilMethods],
           data: function () {
               return {
                   data: {
                       didFinish: false,
                       didTrigger: false,
-                      showMore: false,
-
-                      //sliderIndex:10
+                      userid: '',
+                      token: ''
                   }
               };
           },
-          mounted: function() {
-              $('.ui-p-fixed').removeClass('ui-p-fixed');
+
+          mounted: function () {
+              showLoopScroll();
           },
           created: function () {
               var me = this;
-              try {
-                  if (this.params.rulesContent && this.params.rulesContent.length > 0) {
-                      this.params.rulesContent.forEach(function (item) {
-                          item.rulesDes = item.rulesDes.replace(/\\n/g, "").replace(/\\/g, "").replace(/\\t/g , "");;
-                      });
+              this.params.sliderArr.forEach(function (item) {
+                  if (!item.url) {
+                  item.url = "javascript:;";
                   }
-                  if(this.params.code){
-                      this.params.code = this.params.code.replace(/\\n/g, "").replace(/\\/g, "").replace(/\\t/g , "");
-                  }
-                  if(this.params.sliderArr && this.params.sliderArr.length > 0){
-                      this.params.sliderArr.forEach(function(item){
-                          item.url = me.getJybUrl(item.url);
-                          console.log('item.url',item.url);
-                      });
-                  }
-              } catch (e) {
-                  console.log("去掉rules中的空格和换行符");
-              }
+                 
+              });
           },
           methods: {
               addKeyValue: function (url, key, value) {
-                  url = url.replace(/？/g, "?");//异常处理
-                  var reg = /key[=]/,
-                      hasQuery = /\?/.test(url);
-                  var hasAnchor = url.indexOf('#') > -1;
-                  if (reg.test(url)) {//进行替换
-                      url = url.replace(reg, key + "=" + value);
-                  } else {//没有，则进行追加
-                      url = hasAnchor ? url.replace("#", (hasQuery ? "&" : "?") + key + "=" + value + "#") : (url + (hasQuery ? "&" : "?") + key + "=" + value);
-                  }
+                 
                   return url;
               },
               getJybUrl: function (url) {
-                  var _userid = util.getQuery("userid") || util.getCookie("userid") || "",
-                      _token = util.getQuery("token") || util.getCookie("token") || "";
-                  if (url.indexOf("http") > -1) {
-                      url = this.addKeyValue(url, "userid", _userid);
-                      url = this.addKeyValue(url, "token", _token);
-                  }
+                 
                   return url;
               },
-              hideRulesDialog: function () {
-                  this.params.isShowDialog = true;
-              },
-              showRulesDialog: function () {
-                  this.params.isShowDialog = false;
-              },
-              switchtips: function () {
-                  console.log("switch tips");
-              },
-
-              submitBuy:function(){
-                  //仅仅用于测试，微信还款类组件   真正环境是有js逻辑的  jyb.vue.jybwxhkpay
-              },
-              toDraw:function(){
-
+              jybOpenUrl: function(index){
+                  var _url = this.params.sliderArr[index].url;
+                  openUrl(_url);
               }
 
           }
       });
-
-
-
   }
 
 
