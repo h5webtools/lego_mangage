b4e0e584530c07d2f9f8ced4f6ae356d98bb3198
canye <canye.wu@jyblife.com>
2018-11-19 16:51:27 +0800

    feat: 引入debug资源 #000

--- app/web/pages/debug/app.vue
+++ app/web/pages/debug/app.vue
@@ -1,40 +1,21 @@
 <template>
   <div class="debug-wrap">
-    <div class="debug-header">
-      <el-row class="debug-header-box">
-        <el-col :span="12">
-          <a class="logo" href="javascript:;"><img src="../../assets/img/logo.png"/></a>
-        </el-col>
-        <el-col :span="12" class="ui-ta-r">
-          <el-dropdown class="ui-ta-r">
-            <span class="el-dropdown-link">
-              {{userInfo.userName}}<i class="el-icon-arrow-down el-icon--right"></i>
-            </span>
-            <el-dropdown-menu slot="dropdown">
-              <el-dropdown-item><a href="/login/loginOut">退出登录</a></el-dropdown-item>
-            </el-dropdown-menu>
-          </el-dropdown>
-        </el-col>
-      </el-row>
-    </div>
     <div class="debug-main">
-      <el-row :gutter="20">
-        <el-col :span="10">
-          <div id="js-preview"></div>
-        </el-col>
-        <el-col :span="14">
-          <ace-editor
-            class="debug-editor"
-            editorId="codeString"
-            height="300px"
-            :variable="editorVar"
-            :content="codeString"
-            lang="javascript"
-            @change="handleEditorChange($event)"
-          ></ace-editor>
-          <el-button @click="handleSave" type="primary" size="mini" round>保存</el-button>
-        </el-col>
-      </el-row>
+      <div id="js-preview"></div>
+      <div class="debug-editor-box">
+        <ace-editor
+          class="debug-editor"
+          editorId="codeString"
+          height="300px"
+          :variable="editorVar"
+          :content="codeString"
+          lang="javascript"
+          @change="handleEditorChange($event)"
+        ></ace-editor>
+        <div class="ui-ta-r">
+          <a class="btn-save" href="javascript:;" @click="handleSave">保存</a>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -72,47 +53,42 @@ export default {
   mounted() {
     const handshake = new Postmate({
       container: document.getElementById('js-preview'),
-      url: 'http://172.16.5.59:8887/demo/demo1.html'
+      url: 'http://172.16.5.59:8887/demo1.html'
     });
     handshake.then(child => this.childAPI = child);
   }
 }
 </script>
 <style lang="scss" scoped>
+.ui-ta-r {
+  text-align: right
+}
+
+.debug-wrap {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 9999;
+  overflow: hidden;
+  background-color: #fff;
+}
+
 .debug-main {
+  display: flex;
+  justify-content: space-around;
   width: 1000px;
   margin-left: auto;
   margin-right: auto;
 }
 
-.debug-header {
-  display: flex;
-  align-items: center;
-  height: 50px;
-  background-color: #eef1f6;
-  width: 100%;
-  line-height: 50px;
-  margin-bottom: 18px;
-}
-
-.debug-header-box {
-  width: 100%;
-  padding: 0 20px;
-  box-sizing: border-box;
-  line-height: 1;
-}
-
-.logo {
-  display: inline-block;
-  height: 30px;
-
-  img {
-    height: 30px;
-  }
-}
-
 .debug-editor {
   border: 1px solid #ccc;
+}
+
+.debug-editor-box {
+  width: 600px;
 }
 
 #js-preview {
@@ -123,8 +99,19 @@ export default {
   }
 }
 
-.editor-box {
-  width: 600px;
+.debug-editor {
+  margin-bottom: 10px;
+}
+
+.btn-save {
+  display: inline-block;
+  padding: 4px 8px;
+  background-color: #ed6c44;
+  font-size: 13px;
+  color: #fff;
+  text-align: center;
+  border-radius: 4px;
+  text-decoration: none;
 }
 </style>
 
--- app/web/pages/debug/index.js
+++ app/web/pages/debug/index.js
@@ -1,12 +1,10 @@
 
-import Vue from 'vue';
-import ElementUI from 'element-ui';
 import App from './app.vue';
 
-import './index.scss';
-
-Vue.use(ElementUI, { size: 'small' });
-
-new Vue({
-  render: h => h(App)
-}).$mount('#app');
+window.__debugEditor__ = {
+  install(el, Vue) {
+    new Vue({
+      render: h => h(App)
+    }).$mount(el);
+  }
+};
